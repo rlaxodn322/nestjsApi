705e5dc281ca2240f7a2ed047ede9dfbe172864d
kimtaewoo <rlaxodn322@gmail.com>
2024-11-04 19:00:59 +0900

    dev:start 수정시 서버 재시작 방법 env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [],
-    synchronize: true,
-  }),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,  // 전역 모듈로 설정
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'mysql',  // MySQL 드라이버 사용
+    //     host: configService.get<string>('DB_HOST'),
+    //     port: configService.get<number>('DB_PORT'),
+    //     username: configService.get<string>('DB_USERNAME'),
+    //     password: configService.get<string>('DB_PASSWORD'),
+    //     database: configService.get<string>('DB_NAME'),
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: configService.get<string>('NODE_ENV') !== 'production',
+    //   }),
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
