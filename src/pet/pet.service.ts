b449ef1e615e106cd15a659f60d4e7206d818c55
taewoo <rlaxodn322@gmail.com>
2024-11-08 14:36:43 +0900

    response data filter

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -15,9 +15,31 @@ export class PetService {
     const url = `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?serviceKey=${serviceKey}&pageNo=1&numOfRows=10&_type=json`;
     try {
       const response = await lastValueFrom(this.httpService.get(url));
-      return response.data;
+      return this.filterPetData(response.data);
     } catch (error) {
       throw new Error('Error fetching pet data');
     }
+  }
+  private filterPetData(data: any): any {
+    return data.response.body.items.item.map((item: any) => ({
+      desertionNo: item.desertionNo,
+      happenDt: item.happenDt,
+      happenPlace: item.happenPlace,
+      kindCd: item.kindCd,
+      colorCd: item.colorCd,
+      age: item.age,
+      weight: item.weight,
+      noticeNo: item.noticeNo,
+      noticeSdt: item.noticeSdt,
+      noticeEdt: item.noticeEdt,
+      processState: item.processState,
+      sexCd: item.sexCd,
+      specialMark: item.specialMark,
+      careNm: item.careNm,
+      careTel: item.careTel,
+      careAddr: item.careAddr,
+      orgNm: item.orgNm,
+      chargeNm: item.chargeNm,
+    }));
   }
 }
