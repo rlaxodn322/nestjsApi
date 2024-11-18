f203831c5ce251a1edd2fc2099cf51645ca560b2
taewoo <rlaxodn322@gmail.com>
2024-11-18 13:09:04 +0900

    movie controller test code 작성

--- src/movie/movie.controller.spec.ts
+++ src/movie/movie.controller.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
-
+import { MovieInterface } from './movie.interface';
+import exp from 'constants';
 describe('MovieController', () => {
   let controller: MovieController;
   let service: MovieService;
@@ -47,5 +48,57 @@ describe('MovieController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('findAll', () => {
+    it('', async () => {
+      const result: MovieInterface[] = [];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+  describe('findOne', () => {
+    it('', async () => {
+      const result: MovieInterface = {
+        id: 1,
+        title: 'Test Movie',
+        director: 'Test Director',
+        releaseYear: 2020,
+        description: 'Test Description',
+      };
+      jest.spyOn(service, 'findOne').mockResolvedValue(result);
+      expect(await controller.findOne(1)).toEqual(result);
+    });
+  });
+  describe('create', () => {
+    it('', async () => {
+      const result: MovieInterface = {
+        id: 1,
+        title: 'Test Movie',
+        director: 'Test Director',
+        releaseYear: 2020,
+        description: 'Test Description',
+      };
+      jest.spyOn(service, 'create').mockResolvedValue(result);
+      expect(await controller.create(result)).toEqual(result);
+    });
+  });
+  describe('update', () => {
+    it('', async () => {
+      const result: MovieInterface = {
+        id: 1,
+        title: 'Test Movie',
+        director: 'Test Director',
+        releaseYear: 2020,
+        description: 'Test Description',
+      };
+      jest.spyOn(service, 'update').mockResolvedValue(result);
+      expect(await controller.update(1, result)).toEqual(result);
+    });
+  });
+  describe('remove', () => {
+    it('', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue(undefined);
+      expect(await controller.remove(1)).toBeUndefined();
+    });
   });
 });
